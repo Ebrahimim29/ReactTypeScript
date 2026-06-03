daa5f72bc738b020cf08ec8d7b2c9858929c3f31
Ebrahimim29 <Ebrahimim29@gmail.com>
2026-06-03 06:44:20 +0330

    Events

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
+import ConfirmButton from "./Components/ConfirmButton";
+import InputElement from "./Components/InputElement";
 import Userinfo from "./Components/Userinfo";
 
 const App = () => {
   return (
-    <div>
+    <div className="flex flex-col justify-center items-center m-4">
       <Userinfo name="mohammad" family="ebrahimi" age={30} skills={["laravel"]}>
         <div>
           My Name is Mohammad
           <p>I love Programming</p>
         </div>
       </Userinfo>
+      <ConfirmButton handleClick={(event) => {
+        event.stopPropagation()
+        event.preventDefault()
+        console.log(event);
+      }} />
+      <InputElement handleChange={(event) => {
+        console.log("input value:", event.target.value);
+      }
+      } />
     </div>
   )
 };
